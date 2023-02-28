6b7b806dfd572371d957362ce69920e71d7b5e71
unknown <sumitrawat30aug@gmail.com>
2023-02-28 17:44:26 +0530

    second commit

--- config/database.js
+++ config/database.js
@@ -86,9 +86,9 @@
         user: env('DATABASE_USERNAME', 'postgres'),
         password: env('DATABASE_PASSWORD', 'iamrawat@123'),
         schema: env('DATABASE_SCHEMA', 'public'), // Not required
-        ssl: {
-          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
-        },
+        // ssl: {
+        //   rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+        // },
       },
       debug: false,
     },
